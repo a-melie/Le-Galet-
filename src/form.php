b4bff6f578a905ecf9baaacf3008b10595f7699a
Amelie <a.grn33@gmail.com>
2020-03-25 17:14:40 +0100

    css refactored

--- src/form.php
+++ src/form.php
@@ -46,39 +46,43 @@ if ($_SERVER["REQUEST_METHOD"]=== 'POST') {
 
 
 ?>
-<div class="form">
-    <form class="contact" action="index.php" method="post">
-        <div class="contactus">
+<div>
+    <form action="index.php" method="post">
+        <div class="formElement">
             <label for="name">Nom :</label>
             <?= isset($errors['name1']) ?  $errors['name1'] : '' ?>
-            <input class="item2 contact margintopform form-border" type="text" name="name" id="name" placeholder="Bennet" value="<?= isset($user['name']) ? $user['name']:''?>">
-
+            <input class="inputwidth" type="text" name="name" id="name" placeholder="Bennet" value="<?= isset($user['name']) ? $user['name']:''?>">
+        </div>
+        <div class="formElement">
             <label for="firstname">Prénom :</label>
             <?= isset($errors['firstname1']) ?  $errors['firstname1'] : '' ?>
-            <input class="item2 contact margintopform form-border" type="text" name="firstname" id="firstname" placeholder="Elizabeth" value="<?= isset($user['firstname']) ? $user['firstname']:''?>">
-
+            <input class="inputwidth" type="text" name="firstname" id="firstname" placeholder="Elizabeth" value="<?= isset($user['firstname']) ? $user['firstname']:''?>">
+        </div>
+        <div class="formElement">
             <label for="email">Email :</label>
             <?= isset($errors['email1']) ?  $errors['email1'] : '' ?>
-            <input class="item2 contact margintopform form-border" name="email" id="email" type="email" placeholder="elizabeth@pemberley.com" value="<?= isset($user['email']) ? $user['email']:''?>">
-
+            <input class="inputwidth" name="email" id="email" type="email" placeholder="elizabeth@pemberley.com" value="<?= isset($user['email']) ? $user['email']:''?>">
+        </div>
+        <div class="formElement">
             <label for="phone">Téléphone :</label>
             <?= isset($errors['phone1']) ?  $errors['phone1'] : '' ?>
-            <input class="item2 contact margintopform form-border" name="phone" id="phone" type="tel" placeholder="0632459493" value="<?= isset($user['phone']) ? $user['phone']:''?>">
-
+            <input class="inputwidth" name="phone" id="phone" type="tel" placeholder="0632459493" value="<?= isset($user['phone']) ? $user['phone']:''?>">
+        </div>
+        <div class="formElement">
             <?= isset($errors['subject1']) ?  $errors['subject1'] : '' ?>
-            <select name="subject" id="subject" class="item2 select margintopform form-border">
+            <select class="inputwidth" name="subject" id="subject" >
                 <option value="">Sélectionnez votre motif </option>
                 <option value="reservation"<?= isset($user['subject']) && $user['subject'] === 'reservation' ? 'selected' : '' ?>>Réservation</option>
                 <option value="question" <?= isset($user['subject']) && $user['subject'] === 'question' ? 'selected' : '' ?>>Question(s)</option>
                 <option value="other" <?= isset($user['subject']) && $user['subject'] === 'other' ? 'selected' : '' ?>>Autre</option>
             </select>
-
+        </div>
+        <div class="formElement">
             <label for="message">Votre Message :</label>
             <?= isset($errors['message1']) ?  $errors['message1'] : '' ?>
-            <textarea name="message" id="message" class="item2 message margintopform form-border" placeholder="Lorsqu'un livre est bien écrit, il me semble toujours trop court."><?= isset($user['message']) ? $user['message']:''?></textarea>
-
-            <button class="button margintopform" type="submit">Envoyer !</button>
+            <textarea class="inputwidth" name="message" id="message" placeholder="Lorsqu'un livre est bien écrit, il me semble toujours trop court."><?= isset($user['message']) ? $user['message']:''?></textarea>
         </div>
+        <button type="submit" class="button">Envoyer !</button>
     </form>
     <?= $successMessage;  ?>
 </div>
