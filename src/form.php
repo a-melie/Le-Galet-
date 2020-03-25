33f4cde684603ce4901b33a28aa813b26c184c7b
Amelie <a.grn33@gmail.com>
2020-03-25 11:06:36 +0100

    adjusmtents in form

--- src/form.php
+++ src/form.php
@@ -1,14 +1,18 @@
 <form>
     <form class="contact">
         <div class="contactus">
-            <input class="item2 contact margintopform form-border" type="email" placeholder="@">
-            <input class="item2 contact margintopform form-border" type="text" placeholder="Nom et prénom">
-            <select name="listselect" id="findthem-select" class="item2 select margintopform form-border">
+            <label for="name">Prénom Nom :</label>
+            <input class="item2 contact margintopform form-border" type="text" name="name" id="name"  placeholder="Elizabeth Bennet">
+            <label for="email">Email :</label>
+            <input class="item2 contact margintopform form-border" name="email" id="email" type="email" placeholder="elizabeth@pemberley.com">
+            <select name="subject" id="subject" class="item2 select margintopform form-border">
+                <option value="">Sélectionnez votre motif </option>
                 <option value="reservation">Réservation</option>
                 <option value="question">Question(s)</option>
                 <option value="autre">Autre</option>
             </select>
-            <textarea class="item2 message margintopform form-border" placeholder="Votre message"></textarea>
+            <label for="message">Votre Message :</label>
+            <textarea name="message" id="message" class="item2 message margintopform form-border" placeholder="Lorsqu'un livre est bien écrit, il me semble toujours trop court."></textarea>
             <div class="item2 buttoncontainer">
                 <button class="button margintopform" type="submit">Envoyer !</button>
             </div>
