dba646e9c4c7194e4225c3f2cfea26902b525a99
Amelie <a.grn33@gmail.com>
2020-04-02 12:11:07 +0200

    head refactored

--- edit.php
+++ edit.php
@@ -1,20 +1,8 @@
 <?php
 require_once 'connec.php';
 require_once 'Model/eventModel.php';
-
+require_once 'src/_head.php';
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Le Galet du Jardin Botanique</title>
-    <link rel="shortcut icon" type="image/png" href="images/icons8-cocktail-48.png" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
 <body>
 <nav>
     <a href="index.php" class="logo"><img class="small-img" src="../images/logo-galet.png" alt="logo"></a>
---
+++ src/_head.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le Galet du Jardin Botanique</title>
+    <link rel="shortcut icon" type="image/png" href="images/icons8-cocktail-48.png" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
